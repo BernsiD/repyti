bc9d111a7818f9fcb5d4ab122725c5b8b0753384
Dmitry <d.natxa@gmail.com>
2021-12-15 12:01:46 +0300

    refactor: get revision rules

--- common/tc_session.py
+++ common/tc_session.py
@@ -1,75 +1,42 @@
-import sys
-import requests
+from zeep import Client
+from zeep.transports import Transport
+from lxml import etree
+from requests import Session
 
-from secret import Secret
 
-
-class TC_Session():
-    def __init__(self, username, password, serveradress=Secret.TC_HOST, sessiontoken=None): #Enter your serveradress here
+class TC_Session:
+    def __init__(self, serveradress, username, password): 
         self.username = username
         self.password = password
         self.serveradress = serveradress
-
-    # Login to Teamcenter
+        self.transport = Transport_decoration_free(session=Session())
+        self.client = None
 
     def login(self):
-
-        url = f"{self.serveradress}/tc/services/Core-2006-03-Session?wsdl"
-        headers = {'Content-Type': 'text/xml', 'SOAPAction': 'login'}
-        body = f"""<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ses="http://teamcenter.com/Schemas/Core/2006-03/Session">
-    <soapenv:Header/>
-    <soapenv:Body>
-    <ses:LoginInput username="{self.username}" password="{self.password}" group="" role="" sessionDiscriminator="1i52"/>
-    </soapenv:Body>
-    </soapenv:Envelope>"""
-
-        response = requests.post(url, data=body, headers=headers, verify=False)
-
-        if response.status_code == 200:
-            print('Command Login successful.')
-            print(response.cookies)
-        else:
-            print('Command Login failed.')
-            sys.exit('Command Login operation failed with HTTP Code ' +
-                     str(response.status_code))
-
-        login_session_id = None
-        #Get Session Id wether its ASP.NET or J2EE
-        for cookie in response.cookies:
-            if cookie.name == 'ASP.NET_SessionId':
-                login_session_id = cookie.value
-                token_name = cookie.name
-            elif cookie.name == 'JSESSIONID':
-                login_session_id = cookie.value
-                token_name = cookie.name
-
-        session_token = token_name + "=" + str(login_session_id)
-        print(session_token)
-        self.session_token = session_token
-        return session_token
+        self.client = self.__get_client()
+        resonse = self.client.service.login(username=self.username, password=self.password, group="", role="", sessionDiscriminator="1i52")
 
     def logout(self):
-        #  Logout from Teamcenter
-        # ---------------------------------
-        url = f"{self.serveradress}/tc/services/Core-2006-03-Session?wsdl"
-        headers = {'Content-Type': 'text/xml',
-                   'SOAPAction': 'logout', 'Cookie': self.session_token}
-        body = """<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ses="http://teamcenter.com/Schemas/Core/2006-03/Session">
-    <soapenv:Header/>
-    <soapenv:Body>
-    <ses:LogoutInput/>
-    </soapenv:Body>
-    </soapenv:Envelope>"""
-        response = requests.post(url, data=body, headers=headers, verify=False)
+        self.client.service.logout()
 
-        if response.status_code == 200:
-            print('Command Logout successful.')
-            # print(response.content)
-        else:
-            print('Command Logout failed.')
-            sys.exit('Command Logout failed with HTTP Code ' +
-                     str(response.status_code))
+    def get_transport(self):
+        return self.transport
+
+    def __get_client(self):
+        service = 'Core-2006-03-Session'
+        wsdl = self.__get_wsdl_url(self.serveradress, service)
+        return Client(wsdl, transport=self.transport)
+
+    @staticmethod
+    def __get_wsdl_url(wsdl_host, wsdl_service):
+        return '%s/%s?wsdl' % (wsdl_host, wsdl_service)
 
 
-if __name__ == "__main__":
-    main()
+class Transport_decoration_free(Transport):
+    def post_xml(self, address, envelope, headers):
+        """Post the envelope xml element to the given address with the headers.
+        In this modificaton `xml_declaration` is False to remove <?xml version='1.0' encoding='utf-8'?>
+        For some reason server return empty page with status 200 with this line.
+        """
+        message = etree.tostring(envelope, pretty_print=False, xml_declaration=False, encoding="utf-8")
+        return self.post(address, message, headers)
--- views/get_revision_rules.py
+++ views/get_revision_rules.py
@@ -7,10 +7,9 @@ get_revision_rules_blueprint = Blueprint('get_revision_rules', __name__)
 @get_revision_rules_blueprint.route("/", methods=["GET"])
 def get_revision_rules_view():
     response_dics = get_revision_rules.execute_as_obj()
-    print(response_dics)
     return render_template('revision_rules.html', rule_dics=response_dics)
 
-@get_revision_rules_blueprint.route("/xml", methods=["GET"])
-def get_revision_rules_xml_view():
-    response = get_revision_rules.execute_as_xml()
-    return Response(response, mimetype='text/xml')
+@get_revision_rules_blueprint.route("/json", methods=["GET"])
+def get_revision_rules_json_view():
+    response = get_revision_rules.execute_as_json()
+    return Response(response, mimetype='text/json')
